520d1e35d185a42691c1b88201caac28f01d0ee2
Mark Emrich (open4business GmbH) <mae@o4b.de>
2023-11-16 16:50:06 +0100

    feat: check .npmrc against own file

--- scripts/check-and-update-npmrc.js
+++ scripts/check-and-update-npmrc.js
@@ -1,19 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 
-const npmrcPath = path.join(process.cwd(), '../../../.npmrc');
-const ourNpmrcContent = `# Your npmrc configurations`;
+const sourcePath = path.join(process.cwd(), './.npmrc');
+const targetPath = path.join(process.cwd(), '../../../.npmrc');
+
+// Function to read the .npmrc file and return its content
+function readNpmrcFile() {
+    if (fs.existsSync(sourcePath)) {
+        return fs.readFileSync(sourcePath, 'utf8');
+    } else {
+        console.log('.npmrc file not found.');
+        return null;
+    }
+}
+
+const ourNpmrcContent = readNpmrcFile();
 
 // Check if .npmrc exists
-if (fs.existsSync(npmrcPath)) {
-    let content = fs.readFileSync(npmrcPath, 'utf8');
+if (fs.existsSync(targetPath)) {
+    let content = fs.readFileSync(targetPath, 'utf8');
 
     // Check if our content is already in the .npmrc
     if (!content.includes(ourNpmrcContent)) {
         // Append our content to .npmrc
-        fs.appendFileSync(npmrcPath, `\n${ourNpmrcContent}`);
+        fs.appendFileSync(targetPath, `\n${ourNpmrcContent}`);
     }
 } else {
     // If .npmrc does not exist, create it with our content
-    fs.writeFileSync(npmrcPath, ourNpmrcContent);
+    fs.writeFileSync(targetPath, ourNpmrcContent);
 }
