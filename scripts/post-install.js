2bae5b79792020f3d60ba41f62f3ed825b165e6c
Mark Emrich (open4business GmbH) <mae@o4b.de>
2023-11-16 15:28:28 +0100

    fix: remove folder reference

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -15,9 +15,9 @@ function runScript(scriptPath) {
   }
 }
 
-runScript("./scripts/copy-workflow.js");
-runScript("./scripts/check-and-update-npmrc.js");
-runScript("./scripts/create-release-config.js");
+runScript("./copy-workflow.js");
+runScript("./check-and-update-npmrc.js");
+runScript("./create-release-config.js");
 
 console.log(`Module "${packageJson.name}" has been successfully installed.`);
 
