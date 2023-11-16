d6cedc725878d4df9ff5263ec153cfcfd427c7cd
Mark Emrich (open4business GmbH) <emrich@open4business.de>
2023-11-16 12:21:42 +0100

    feat: rename to be created releaserc file

--- scripts/create-releaserc.js
+++ scripts/create-releaserc.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const releaseConfigPath = path.join(process.cwd(), 'release.config.js');
+const releaseConfigPath = path.join(process.cwd(), '.releaserc');
 const releaseConfigContent = `// Configuration for semantic release
-module.exports = {
-  ...require('@open4business/semantic-versioning')
-};
+{
+  "extends": "@open4business/semantic-versioning"
+}
 `;
 
 // Create release.config.js with the specified content
