2d06bd2307ffdb9936d8611e7e5d59327d144de9
Mark Emrich (open4business GmbH) <mae@o4b.de>
2024-02-08 07:46:03 +0100

    refactor: directory creation and file copying

--- scripts/copy-workflows.js
+++ scripts/copy-workflows.js
@@ -5,12 +5,12 @@ const sourceDir = path.join(process.cwd(), './.github/workflows');
 const targetDir = path.join(process.env.INIT_CWD, './.github/workflows');
 
 function copyRecursively(srcDir, tgtDir) {
-  // Ensure the target directory exists
+  // Ensure target directory exists
   if (!fs.existsSync(tgtDir)) {
     fs.mkdirSync(tgtDir, { recursive: true });
   }
 
-  // Read the contents of the current directory
+  // Read contents of the current directory
   fs.readdirSync(srcDir).forEach((item) => {
     const srcItem = path.join(srcDir, item);
     const tgtItem = path.join(tgtDir, item);
