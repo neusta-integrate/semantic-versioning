0872216801c700fd311b644b272b2a1a9d958d54
Mark Emrich (open4business GmbH) <mae@o4b.de>
2024-02-08 14:58:38 +0100

    fix: reset to current branch for publish

--- .github/workflows/scripts/read-current-version.sh
+++ .github/workflows/scripts/read-current-version.sh
@@ -2,11 +2,17 @@
 
 # Reads the current version number from package.json of the main branch and exports it as an environment variable
 
+# Save the current branch name
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 # Checkout the main branch to ensure we're reading the package.json from the main branch
 git checkout main
 
 # Extract the current version from package.json of the main branch
 CURRENT_VERSION=$(git show main:package.json | jq -r '.version')
+
+# Switch back to the original branch
+git checkout $CURRENT_BRANCH
 
 # Display the current version
 echo "Current version number: $CURRENT_VERSION"
